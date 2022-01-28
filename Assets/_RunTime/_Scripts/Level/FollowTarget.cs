3c9daaff685cd51d5e3db9c0afb8c0621898226f
Arthur Stefanelli <artrexwow@gmail.com>
2022-01-28 15:54:36 -0300

    UI

--- Assets/_RunTime/_Scripts/Level/FollowTarget.cs
+++ Assets/_RunTime/_Scripts/Level/FollowTarget.cs
@@ -1,14 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 
 [ExecuteAlways]
 public class FollowTarget : MonoBehaviour
 {
     [SerializeField] private Transform _target;
+    [SerializeField] private float _offSet;
     private void LateUpdate()
     {
-        transform.position = new Vector3(_target.position.x, transform.position.y, transform.position.z);
+        transform.position = new Vector3(_target.position.x + _offSet, transform.position.y, transform.position.z);
     }
 }
